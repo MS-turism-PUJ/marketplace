35a76dc0f481a47e4cb02a2fe447935f415d2aaa
Santicm23 <santicastrom@outlook.com>
2024-09-12 21:27:55 -0500

    init project

---
+++ src/test/java/Payment/MS/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package Payment.MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
