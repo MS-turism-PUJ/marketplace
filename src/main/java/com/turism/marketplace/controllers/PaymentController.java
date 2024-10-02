66a64d0085fafd949cb1afb090d847d87963d9b9
Santicm23 <santicastrom@outlook.com>
2024-10-02 08:03:13 -0500

    fixing header bug

--- src/main/java/com/turism/marketplace/controllers/PaymentController.java
+++ src/main/java/com/turism/marketplace/controllers/PaymentController.java
@@ -2,23 +2,32 @@ package com.turism.marketplace.controllers;
 
 import java.util.List;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.stereotype.Controller;
 
 import com.turism.marketplace.models.Payment;
 import com.turism.marketplace.models.User;
 import com.turism.marketplace.repositories.PaymentRepository;
 import com.turism.marketplace.repositories.UserRepository;
 
+@Controller
 public class PaymentController {
-    @Autowired
     private PaymentRepository paymentRepository;
     private UserRepository userRepository;
+    private HttpServletRequest request;
+
+    @Autowired
+    public PaymentController(PaymentRepository paymentRepository, UserRepository userRepository, HttpServletRequest request) {
+        this.paymentRepository = paymentRepository;
+        this.userRepository = userRepository;
+        this.request = request;
+    }
 
     @QueryMapping
-    public List<Payment> findAllByUser(@RequestHeader("X-Preferred-Username") String preferredUsername) {
-        System.out.println("User: " + preferredUsername);
+    public List<Payment> findAllByUser() {
+        String preferredUsername = request.getHeader("X-Preferred-Username");
         User user = userRepository.findByUsername(preferredUsername);
         return paymentRepository.findByUser(user);
     }
